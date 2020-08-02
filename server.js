4dec1cbaf94e7a27268f40dbcfe3c0bdbbfe34c5
Maksim Verkhoturov <sd32@protonmail.com>
2020-08-02 04:32:16 -0600

    adding main server.js logic

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(require("./controllers/burgers_controller"));
+
+app.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`Listening on localhost:${PORT}`);
+});
